4bec3a67864cc2cdb4e3aeaf4014d541a94c22c1
Sebastian Heunke <sebastian.heunke@hm.edu>
2018-11-19 17:09:48 +0100

    getLocation function to send rest request.

--- src/MapApi.hs
+++ src/MapApi.hs
@@ -2,19 +2,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 module MapApi where
 
-import System.Environment
-import Data.Text
-import Network.Wreq
-import Control.Lens
+import System.Environment -- to get environment variables
+import Data.Text -- to pack strings to pass into lib functions
+
+import Network.Wreq -- Main REST request lib
+import Control.Lens -- Lib used to access results from requests
+import Data.Aeson.Lens -- Json Access
 
 --TODO: Add setup information about env-var to top level readme
 
+data Result = Location { name :: Text,
+                        entityType :: Text,
+                        confidence :: Text}
 
+-- Print a location result
+instance Show Result where
+    show (Location name entityType confidence) = unpack name ++ " (" ++ unpack entityType ++ ", Confidence: " ++ unpack confidence ++ ")"
+
+
+-- Main node of the rest request
 mapsUrl = "http://dev.virtualearth.net/REST/v1/Locations"
 
-tmp = do
-    apiKey <- getEnv("BING_API_KEY")
-    opts <- return(defaults & param "query" .~ ["donau"]
-                    & param "key" .~ [pack(apiKey)])
+sendQuery query =
+    do
+    apiKey <- getEnv "BING_API_KEY"
+    let opts = defaults & param "query" .~ [pack query]
+                & param "key" .~ [pack apiKey]
+         in return(getWith opts mapsUrl)
+
+
+
+getLocation query  = do
+    apiKey <- getEnv "BING_API_KEY"
+    opts <- return(defaults & param "query" .~ [pack(query)]
+                    & param "key" .~ [pack apiKey])
     r <- getWith opts mapsUrl
-    return(r ^. responseBody)
+    -- Todo find a way to reuse partial results (like the resource node)
+    return( Location (r^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth 0 . key "name" . _String)
+                     (r^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth 0 . key "entityType" . _String)
+                     (r^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth 0 . key "confidence" . _String))

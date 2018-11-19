9a33774e2818274f2ffe4b9f07dcd35b486c1829
Sebastian Heunke <sebastian.heunke@hm.edu>
2018-11-19 11:01:10 +0100

    Examples Map Request wtih envvar.

--- src/MapApi.hs
+++ src/MapApi.hs
@@ -2,9 +2,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module MapApi where
 
+import System.Environment
+import Data.Text
 import Network.Wreq
 import Control.Lens
 
+--TODO: Add setup information about env-var to top level readme
+
+
+mapsUrl = "http://dev.virtualearth.net/REST/v1/Locations"
+
 tmp = do
-    r <- get "http://httpbin.org/get"
+    apiKey <- getEnv("BING_API_KEY")
+    opts <- return(defaults & param "query" .~ ["donau"]
+                    & param "key" .~ [pack(apiKey)])
+    r <- getWith opts mapsUrl
     return(r ^. responseBody)

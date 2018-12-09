99cd4f96af2f9360517df3f66bc77b29b7e3bb7e
Sebastian Heunke <sebastian.heunke@hm.edu>
2018-12-09 16:59:01 +0100

    small changes.

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -1,4 +1,8 @@
-module GameLogic (checkCity, currentScores) where
+module GameLogic (checkCity, checkRiver, checkCountry, currentScores, score) where
+
+import Data.IORef
+score :: IORef Int
+score = return (newIORef 0)
 
 checkCity :: [string] -> [(Int, String)]
 checkCity cities = [(42, "high"), (0, "invalid"), (20, "high"), (10, "low")]

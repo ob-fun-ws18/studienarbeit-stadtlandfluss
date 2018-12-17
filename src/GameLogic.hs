5255ff99c6bfaa5d470275c8930e3ad5b41e075f
Sebastian Heunke <sebastian.heunke@hm.edu>
2018-12-17 13:35:48 +0100

    Haddok Comments for GameLogic and MapApi.

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -1,3 +1,11 @@
+{-|
+Module      : GameLogic
+Description : Contains functions to score possible Answers
+
+This module contains the "Brain" of the Game. It allows input to be checked using the MapApi Lib
+and it allows the scoring of answers.
+-}
+
 module GameLogic where --(checkCity, checkCountry, checkRiver, currentScores) where
 
 import MapApi
@@ -6,31 +14,49 @@ import Data.Maybe
 
 -- Valid types are: CountryRegion (Land), River (Fluss), PopulatedPlace (Stadt). For others see bing api doc
 
+-- TODO: replace by ...Io version
 checkCity :: [string] -> [(Int, String)]
 checkCity cities = [(42, "high"), (0, "invalid"), (20, "high"), (10, "low")]
 
+-- TODO: replace by ...Io version
 checkCountry :: [string] -> [(Int, String)]
 checkCountry cities = [(42, "high"), (0, "invalid"), (20, "high"), (10, "low")]
 
+-- TODO: replace by ...Io version
 checkRiver :: [string] -> [(Int, String)]
 checkRiver cities = [(42, "high"), (0, "invalid"), (20, "high"), (10, "low")]
 
-checkRiverIo :: [String] -> IO [ (Int, String)]
+-- | Checks and scores a list of answers for the river type
+checkRiverIo :: [String] -- ^ List of all answers given
+             -> IO [ (Int, String)] -- ^ Score and likelihood of every answer
 checkRiverIo answers =  mapM (evaluateAnswer "River" answers) answers
 
-checkCountryIo :: [String] -> IO [ (Int, String)]
+-- | Checks and scores a list of answers for the country type
+checkCountryIo :: [String] -- ^ List of all answers given
+               -> IO [ (Int, String)] -- ^ Score and likelihood of every Answer
 checkCountryIo answers =  mapM (evaluateAnswer "CountryRegion" answers) answers
 
-checkCityIo :: [String] -> IO [ (Int, String)]
+-- | Checks and scores a list of answers for the city type
+checkCityIo :: [String] -- ^ List of all answers given
+            -> IO [ (Int, String)] -- ^ Score and likelihood of every Answer
 checkCityIo answers =  mapM (evaluateAnswer "PopulatedPlace" answers) answers
 
-evaluateAnswer :: String -> [String] -> String -> IO (Int, String)
+-- | Evaluates how likely a given answer is to be correct and how many points it should be worth
+-- Uses the Rest API to get all Locations matching the Answer.
+evaluateAnswer :: String -- ^ The entity type of the answer CountryRegion | River | PopulatedPlace
+               -> [String] -- ^ List of all Answers to determine score multiplier
+               -> String -- ^ The given Answer which will be scored
+               -> IO (Int, String) -- ^ Score and likelihood
 evaluateAnswer queryType allQueries query =
         do locations <- getLocation query
            return (scoreAnswer queryType locations query allQueries)
 
-
-scoreAnswer :: String -> [Location] -> String -> [String] -> (Int, String)
+-- | Score a answer based on given locations
+scoreAnswer :: String -- ^ Entity Type which the location should have
+            -> [Location] -- ^ List of locations matching the query
+            -> String -- ^ Query / Answer to be scored
+            -> [String] -- ^ All Answers used for score multiplier
+            -> (Int, String) -- ^ Score and likelihood
 scoreAnswer queryType locs query allQueries
     = let loc = bestMatch queryType locs
           conf = maybe "Invalid" confidence loc
@@ -38,21 +64,29 @@ scoreAnswer queryType locs query allQueries
           bonusFactor = calcBonusFactor query allQueries
       in (points * bonusFactor, conf)
 
-bestMatch :: String -> [Location] -> Maybe Location
+-- | Filter a sorted List of locations for the best match in a given entity Type
+bestMatch :: String -- ^ Entity type which the result MUST match
+          -> [Location] -- ^ List of all Locations
+          -> Maybe Location -- ^ Best Match for given type, nothing if no result for given type was found
 bestMatch expectedType locations = listToMaybe (filter (isType expectedType) locations)
 
-isType :: String -> Location -> Bool
+-- | determine if Location type matches
+isType :: String -- ^ Name of the type expected
+       -> Location -- ^ Location to be checked
+       -> Bool -- ^ true if entityType is as expected
 isType expetected location = entityType location == expetected
 
-calcBonusFactor :: String -> [String] -> Int
+-- | Calculates a score multiplier for a given Answer
+-- 4: if only one non empty answer was given. (No one else answered in that category)
+-- 2: The answer is unique. (There where other answers, but not equal answers)
+-- 1: The answer was given multiple times.
+-- Note: Empty answers will get score multipliers too, but since they will receive 0 points the multiplier does not matter
+calcBonusFactor :: String -- ^ Answer for which the multiplier is calculated
+                -> [String] -- ^ List of all Answers (including the one being evaluated)
+                -> Int -- ^ Score multiplier (1 | 2 | 4)
 calcBonusFactor query allQueries | length (filter (== query) allQueries) > 1 = 1 -- Duplicate answer
                                  | length (filter (== "") allQueries) == length allQueries - 1 = 4 -- Only answer
                                  | otherwise = 2 -- Unique answer
-
--- currently unused, if too many false answer are accepted filtering out "low" results might help
-hasConfidence :: String -> Location -> Bool
-hasConfidence expected location = confidence location == expected
-
 
 
 currentScores :: [Int]
--- src/MapApi.hs
+++ src/MapApi.hs
@@ -1,6 +1,12 @@
 
 {-# LANGUAGE OverloadedStrings #-}
-module MapApi where --(Location(..), getLocation) where
+{-|
+Module      : MapApi
+Description : Contains functions to access the Bing Map Api
+
+This module handles accessing the Bing Maps api to return Locations for a given Query.
+-}
+module MapApi (Location(..), getLocation) where
 
 import System.Environment -- to get environment variables
 import Data.Text -- to pack strings to pass into lib functions
@@ -9,22 +15,24 @@ import Network.Wreq -- Main REST request lib
 import Control.Lens -- Lib used to access results from requests
 import Data.Aeson.Lens -- Json Access
 
-
-data Location = Location { name :: String,
-                        entityType :: String,
-                        confidence :: String}
+-- | Location describing a rest request result
+data Location = Location { name :: String, -- ^ Name of the location found
+                        entityType :: String, -- ^ Entitiy Type of the location (e.g. River)
+                        confidence :: String -- ^ how confident the Api was that this is the correct result (low, medium or high)
+                        }
 
 -- Print a location result
 instance Show Location where
     show (Location name entityType confidence) = "{" ++ name ++ " (" ++ entityType ++ ", Confidence: " ++ confidence ++ ")}"
 
 
--- Main node of the rest request
+-- | Main node of the rest request
 mapsUrl :: String
 mapsUrl = "http://dev.virtualearth.net/REST/v1/Locations"
 
-
-getLocation :: String -> IO [Location]
+-- | Get a list of all matches for the given query string
+getLocation :: String -- ^ Query to be searched
+            -> IO [Location] -- ^ List of all locations returned from the api. Can be empty
 getLocation query  = if query == ""
                         then return []
                         else
@@ -40,20 +48,25 @@ getLocation query  = if query == ""
                                     range =  [0..numResult-1]
                                 return (Prelude.map (readLocation response) range)
 
--- Access functions for Location-json fields
--- These are not typed because the types rely on the Json implementation which might vary
-
-readName response index = response ^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth index . key "name" . _String
-
-readEntityType response index =  response ^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth index . key "entityType" . _String
-
-readConfidence response index =  response ^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth index . key "confidence" . _String
-
-numberOfResources response = response ^.. responseBody . key "resourceSets" . nth 0 . key "estimatedTotal"  . _Integer
-
+-- | Read in all relevant info from the given json to a Location Data object
 readLocation response index = Location
                                 (unpack (readName response index))
                                 (unpack (readEntityType response index))
                                 (unpack (readConfidence response index))
+
+-- Access functions for Location-json fields
+-- These are not typed because the types rely on the Json implementation which might vary
+
+-- | Read the name of the nth Location from the response body
+readName response index = response ^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth index . key "name" . _String
+
+-- | Read the entity ype of the nth Location from the response body
+readEntityType response index =  response ^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth index . key "entityType" . _String
+
+-- | Read the confidence of the nth Location from the response body
+readConfidence response index =  response ^. responseBody . key "resourceSets" . nth 0 . key "resources" . nth index . key "confidence" . _String
+
+-- | Read the info field in which the number of given responses is stored
+numberOfResources response = response ^.. responseBody . key "resourceSets" . nth 0 . key "estimatedTotal"  . _Integer
 
 
